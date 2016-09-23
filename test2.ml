f476a3b639c34dc882e948770374f89927ae23ea
dodo <dbousque@student.42.fr>
2016-09-23 05:29:42 +0200

    switched to pipes, a tad slower but reliable, started building interfaces

---
+++ test2.ml
@@ -0,0 +1,48 @@
+
+
+let read_pipe_name = "my_named_pipe"
+let write_pipe_name = "my_named_pipe2"
+
+let create_pipe name =
+	try Unix.mkfifo name 0o777 with
+		| Unix.Unix_error _ -> ignore ()
+
+let get_pipes name_read name_write =
+	(* set O_SYNC to have synchronous (unbuffered) communication,
+	   so we don't have to flush, maybe O_DSYNC instead ? *)
+	let fd_read = Unix.openfile name_read [Unix.O_RDONLY, O_SYNC] 0o777 in
+	let fd_write = Unix.openfile name_write [Unix.O_WRONLY, O_SYNC] 0o777 in
+	(fd_read, fd_write)
+
+let communicate fd_read fd_write process_stdout =
+	let bytes = Bytes.create 10 in
+	let inp_bytes = Bytes.create 10 in
+	let oldstdout = Unix.dup Unix.stdout in
+	Bytes.set bytes 0 (Char.chr 6) ;
+	Bytes.set bytes 1 's' ;
+	Bytes.set bytes 2 'a' ;
+	Bytes.set bytes 3 'l' ;
+	Bytes.set bytes 4 'u' ;
+	Bytes.set bytes 5 't' ;
+	Bytes.set bytes 6 '\n' ;
+	while true do
+		ignore (Unix.write fd_write bytes 0 1) ;
+		ignore (Unix.write fd_write bytes 0 6) ;
+		print_endline "waiting for nb_to_read" ;
+		ignore (Unix.read fd_read inp_bytes 0 1) ;
+		print_endline "got it" ;
+		ignore (Unix.read fd_read inp_bytes 0 (Char.code (Bytes.get inp_bytes 0))) ;
+		print_string "got : " ;
+		print_endline inp_bytes
+	done
+
+let create_process () =
+	let (in_c, out_c) = Unix.open_process "python3 test2.py" in
+	out_c
+
+let () =
+	create_pipe read_pipe_name ;
+	create_pipe write_pipe_name ;
+	let process_stdout = create_process () in
+	let (read_pipe, write_pipe) = get_pipes read_pipe_name write_pipe_name in
+	communicate read_pipe write_pipe process_stdout
---
+++ test2.py
@@ -0,0 +1,50 @@
+
+
+READ_PIPE_NAME = "my_named_pipe2"
+WRITE_PIPE_NAME = "my_named_pipe"
+
+from time import time
+from struct import pack, unpack
+
+class Log:
+
+	def __init__(self, filename="python_log"):
+		self.file = open(filename, "w")
+
+	def log(self, msg):
+		self.file.write(msg)
+
+	def close(self):
+		self.file.close()
+
+class PipeReaderWriter:
+
+	def __init__(self, read_pipe_name, write_pipe_name):
+		self.set_pipes(read_pipe_name, write_pipe_name)
+		self.say_hello()
+
+	def get_pipes(self, read_pipe_name, write_pipe_name):
+		# order of open matters, since it is blocking, should match OCaml order
+		# 0 to be unbuffered, so we don't have to flush (better performance ?)
+		self.write_pipe = open(write_pipe_name, 'wb', 0)
+		self.read_pipe = open(read_pipe_name, 'rb', 0)
+		return (read_pipe, write_pipe)
+
+	def send_bytes(self, byts):
+		# '=Q' to force unsigned 8 bytes integer
+		self.write_pipe.write(pack('=Q', len(byts)))
+		#self.write_pipe.flush()
+		self.write_pipe.write(byts)
+		#self.write_pipe.flush()
+
+	def get_bytes(self):
+		# '=Q' to force unsigned 8 bytes integer
+		nb_bytes = unpack('=Q', self.read_pipe.read(8))[0]
+		return self.read_pipe.read(nb_bytes)
+
+	def say_hello(self):
+		# being polite can't make things worse
+		self.send_bytes(b'hello')
+
+log = Log()
+pipes = PipeReaderWriter(READ_PIPE_NAME, WRITE_PIPE_NAME)
